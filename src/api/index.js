6a3b6eabdf4a89788f374df08326036db5793446
Aaditya Doiphode <aadid2004@gmail.com>
2021-05-19 12:54:05 -0700

    added infected and deaths chart and country picker

--- src/api/index.js
+++ src/api/index.js
@@ -9,11 +9,32 @@ export const fetchData = async () => {
     } = await axios.get(url);
 
     return { confirmed, recovered, deaths, lastUpdate };
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const fetchDailyData = async () => {
   try {
     const { data } = await axios.get(`${url}/daily`);
+
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
   } catch (error) {}
 };
